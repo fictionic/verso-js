bd03dafa29b74cf0812f69e834c7b394f2d1f8b5
Dylan Forbes <dylandforbes@gmail.com>
2026-03-20 08:24:25 -0700

    limit RootContainer props to only ones that produce html output

--- src/sluice/core/components/RootContainer.tsx
+++ src/sluice/core/components/RootContainer.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { PAGE_ELEMENT_TOKEN_ID_ATTR } from '../../constants';
 
-export interface RootContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+type RenderableHTMLAttributes = &
+  Pick<
+    React.HTMLAttributes<HTMLDivElement>,
+    'id' | 'className' | 'style' | 'role' | 'hidden' | 'title' | 'lang' | 'dir' | 'tabIndex'
+  > &
+  React.AriaAttributes & {
+  [key: `data-${string}`]: string | number | boolean | undefined;
+};
+
+export type RootContainerProps = RenderableHTMLAttributes & {
   children?: React.ReactNode;
 }
 
@@ -12,12 +21,14 @@ export default function RootContainer(_: RootContainerProps): React.ReactNode {
 
 export type RootContainerElementType = React.ReactElement<RootContainerProps>;
 
+const DIV_CLOSE = '</div>';
+
 export function renderContainerOpen(element: RootContainerElementType, index: number): string {
   const { children, ...attrs } = element.props;
   const html = renderToString(<div {...{[PAGE_ELEMENT_TOKEN_ID_ATTR]: String(index)}} {...attrs} />);
-  return html.slice(0, -('</div>'.length)) + '\n';
+  return html.slice(0, -(DIV_CLOSE.length)) + '\n';
 }
 
 export function renderContainerClose(): string {
-  return `</div>\n`;
+  return `${DIV_CLOSE}\n`;
 }
